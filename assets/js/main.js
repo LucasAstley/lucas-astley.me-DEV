588b80e011a3e0974c673c848b93e263591c9fc5
-Lucas <lucas.llasbennes@gmail.com>
2023-07-24 03:10:06 +0200

    Some changes
    HTML:
    + Add navigation underline animation
    + Add secret hidden link
    + Change contact 'message' section
    + Add light & dark mode button
    
    CSS:
    + Change buttons hover shadow color
    + Center contact send button
    
    JS:
    + Start light & dark mode switch functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,3 +54,61 @@ sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{});
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+
+/*==================== LIGHT & DARK MODE SWITCH ====================*/
+// Element
+var darkmodeBtn = document.getElementById("darkmode-btn");
+var btnIcon = document.getElementById("btn-icon");
+var bgMask = document.getElementById("bg-mask");
+var mainBg = document.getElementById("main");
+
+// Element Content
+var darkColor = "#0e0e0e";
+var lightColor = "#fff";
+
+var bgDark = "#191919";
+var bgLight = "#f2f2f2";
+
+// Status
+var status = 0; // 0 = Light mode, 1 = Dark mode
+var duration = 400;
+
+function switchMode() {
+
+    if (status == 0) {
+
+        bgMask.style.backgroundColor = darkColor;
+        setTimeout(function() {
+            mainBg.style.backgroundColor = darkColor;
+        }, duration);
+        btnIcon.setAttribute("class", "bx bx-moon");
+        btnIcon.style.color = lightColor;
+        darkmodeBtn.style.backgroundColor = bgDark;
+        
+        status = 1;
+
+    } else if (status == 1) {
+
+        bgMask.style.backgroundColor = lightColor;
+        setTimeout(function() {
+            mainBg.style.backgroundColor = lightColor;
+        }, duration);
+        btnIcon.setAttribute("class", "fas fa-sun fa-2x");
+        btnIcon.style.color = darkColor;
+        darkmodeBtn.style.backgroundColor = bgLight;
+
+        status = 0;
+
+    }
+
+    restartAnimation();
+
+}
+
+function restartAnimation() {
+
+    bgMask.classList.remove("animation");
+    void bgMask.offsetWidth;
+    bgMask.classList.add("animation");
+    
+}
